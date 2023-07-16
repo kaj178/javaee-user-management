7e3e1f1ba8ab9fa149c85ad649d574664a361281
Anh Khoa <anhkhoa5525@gmail.com>
2023-07-16 20:35:22 +0700

    Fetch API and not render yet

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
-});
+	$('[data-toggle="tooltip"]').tooltip()
+})
 
-let tableBody = document.querySelector("tbody");
+let tableBody = document.querySelector("tbody")
 // console.log(tableBody);
 fetch ("http://localhost:8080/demorest/api/users/v1")
 	.then (response => response.json())
 	.then(data => {
-		data.forEach(user => {
-			console.log(user.id)
+		console.log(data)
+		let tableInfo = ""
+		data.data.forEach(user => {
+			console.log(user.gender)
+			tableInfo += `
+				<tr>
+					<td>${user.id}</td>
+					<td>${user.name}</td>
+					<td>${user.gender}</td>
+					<td>${user.status}</td>
+					 <td>
+						<a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+						<a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                     </td>
+			`
 		})
+		tableBody.append(tableInfo)
 	})
-	.catch(error => console.log(error));
+	.catch(error => console.log(error))
