39c3bf0403be93d6eebb2b7631425a0a8eeec90f
Anh Khoa <anhkhoa5525@gmail.com>
2023-07-17 14:22:20 +0700

    Load data to table

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,17 +1,16 @@
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip()
 
-let tableBody = document.querySelector("tbody")
-// console.log(tableBody);
-fetch ("http://localhost:8080/demorest/api/users/v1")
-	.then (response => response.json())
-	.then(data => {
-		console.log(data)
-		let tableInfo = ""
-		data.data.forEach(user => {
-			console.log(user.gender)
-			tableInfo += `
+    let tableBody = document.querySelector("tbody")
+    // console.log(tableBody);
+    fetch("http://localhost:8080/demorest/api/users/v1")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let tableInfo = ""
+            data.data.forEach(user => {
+                // console.log(user.gender)
+                tableInfo += `
 				<tr>
 					<td>${user.id}</td>
 					<td>${user.name}</td>
@@ -21,8 +20,12 @@ fetch ("http://localhost:8080/demorest/api/users/v1")
 						<a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
 						<a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
                      </td>
+                </tr>
 			`
-		})
-		tableBody.append(tableInfo)
-	})
-	.catch(error => console.log(error))
+            })
+            // console.log(tableInfo)
+            tableBody.innerHTML = tableInfo
+        })
+        .catch(error => console.log(error))
+})
+
